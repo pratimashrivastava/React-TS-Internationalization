9aaed220422792ad5b5a3b0c96ba012b7b7cc879
Chandan Kumar <chandankumar.r@gmail.com>
2020-05-13 22:01:20 +0530

    i18n fixed

--- i18n.ts
+++ i18n.ts
@@ -1,12 +1,28 @@
-const NextI18Next = require('next-i18next/dist/commonjs').default
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-const NextI18NextInstance = new NextI18Next({
-  defaultLanguage: 'en',
-  otherLanguages: ['hi'],
-  // localeSubpaths: 'foreign', // locale subpaths for url could be none, foreign or all
-});
-module.exports =  NextI18NextInstance
-module.exports =  {
-  appWithTranslation,
-  withNamespaces,
-} = NextI18NextInstance;
+// the translations
+// (tip move them in a JSON file and import them)
+const resources = {
+  en: {
+    translation: {
+      "Welcome to React": "Welcome to React and react-i18next",
+      "hello-world": "Hello, World!",
+    },
+  },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { i18n, Link, withNamespaces } from '../i18n' // We replace next/link with the one provide by next-i18next, this helps with locale subpaths
-
-const HomePage: React.FunctionComponent = ({ t }) => (
-  <div>
-    <h1>{t('hello-world')}</h1>
-    <button type='button' onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'hi' : 'en')}>{t('change-language')}</button>
-    <Link href='/second-page'><button type='button'>{t('goto-second-page')}</button></Link>
+import React from "react";
+import "../i18n";
+import { useTranslation } from "react-i18next";
+// import { i18n, Link, withNamespaces } from "../i18n"; // We replace next/link with the one provide by next-i18next, this helps with locale subpaths
+const HomePage: React.FunctionComponent = () => {
+  const { t, i18n } = useTranslation();
+  return (
+    <div>
+      <h1>{t("hello-world")}</h1>
+      <button
+        type="button"
+        onClick={() =>
+          i18n.changeLanguage(i18n.language === "en" ? "hi" : "en")
+        }
+      >
+        {t("change-language")}
+      </button>
     </div>
-      )
+  );
+};
 
-HomePage.getInitialProps = () => ({
-        namespacesRequired: ['common'],
-})
-
-export default withNamespaces('common')(HomePage)
+export default HomePage;
